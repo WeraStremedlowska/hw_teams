7038d57dcf0524d19d89204ca5bdb9d55db2b699
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-03-06 13:12:45 +0100

    added two classes

---
+++ src/main/java/PrintDayOfTheWeekWithTheNumber.java
@@ -0,0 +1,23 @@
+public class PrintDayOfTheWeekWithTheNumber { //Виктория К
+
+    //Написать метод, который принимает на вход название дня недели и возвращает порядковый номер от 1 до 7,
+    // соответствующий данному дню недели
+    public static int getDayOfTheWeek(String dayName) {
+
+        String[] dayOfTheWeek = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+        for (int i = 0; i < dayOfTheWeek.length; i++) {
+            if (dayOfTheWeek[i].equals(dayName)) {
+                return +i;
+            }
+        }
+        return -1;
+    }
+
+    public static int printDayNumber(int dayNumber) {
+        if (dayNumber != -1) {
+            return dayNumber;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/test/java/PrintDayOfTheWeekWithTheNumberTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class PrintDayOfTheWeekWithTheNumberTest {
+
+    @Test
+    public  void HappyPassDayOfTheWeekWithNumberTest(){
+
+    }
+}
