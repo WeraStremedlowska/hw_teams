cb3b5d1b9970169d697c30ce9b12608efe61e723
new_name <new_email>
2024-03-06 21:59:45 +0100

    Created tests classes and writed tests

---
+++ src/test/java/StorePriceComparatorTest.java
@@ -0,0 +1,14 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StorePriceComparatorTest {
+    @Test  // Olga Safronova
+    public void testHappyPathFindMinPrice() {
+        String[] storePrices =
+                {"Store А: 500 ","StoreB: 200", "StoreC: 100", "StoreD 175", "StoreE 130"};
+        String expected = "StoreC with Minimum Price: 100";
+        String actual = StorePriceComparator.findMinimumPriceStore(storePrices);
+
+        Assert.assertEquals(actual, expected);
+    }
+}
