ba1a7100b8c9715eff4488d7fee788c13f628f1e
ViktoriiaKarpus <vikakarpys@gmail.com>
2024-03-09 10:00:40 +0100

    Added new tests

--- src/test/java/DayOfWeek_19Test.java
+++ src/test/java/DayOfWeek_19Test.java
@@ -4,12 +4,21 @@ import org.testng.annotations.Test;
 public class DayOfWeek_19Test {
 
     @Test
-    public void testHappyDayOfTheWeek(){
+    public void testHappyDayOfTheWeek() {
 
-        String expectedResult = DayOfWeek_19.getDayOfWeekChooser(2);
-        String actualResult = DayOfWeek_19.getDayOfWeekChooser2(2);
+        String expectedResult = "Tuesday";
+        String actualResult = DayOfWeek_19.getDayOfWeekChooser(2);
 
         Assert.assertEquals(actualResult, expectedResult);
     }
 
+    @Test
+    public void testInvalid() {
+
+        String expectedResult = "Invalid day of week number ";
+        String actualResult = DayOfWeek_19.getDayOfWeekChooser(8);
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
 }
